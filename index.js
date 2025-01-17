ce7b53b6002c82e7d219f0087c0bf319e3c60a06
Drushti Chepte <cheptedrushti@gmail.com>
2025-01-17 19:09:19 +0530

    deploy process

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ app.use(
   })
 );
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT || 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
 db.connect();
 export default db;
 
